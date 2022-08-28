101921355a7249ec847d751be674664c72cda5e2
pingleee <v65224@gmail.com>
2022-08-28 18:25:57 +0900

    refactor

--- products.py
+++ products.py
@@ -1,32 +1,50 @@
 import os # operating system
 
-products = []
-# find file
-if os.path.isfile('products.csv'):
 # read file
-    with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
         for line in f:
             if '商品,價格' in line:
-                continue
+                     continue
             name, price = line.strip().split(',')
             products.append([name, price])
-    print(products)
-
+    return products
+    
 # user enter the information
-while True:
-    name = input('Enter your product name: ')
-    if name == 'q':
-        break
-    price = input('Enter the product price: ') 
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('Enter your product name: ')
+        if name == 'q':
+            break
+        price = input('Enter the product price: ') 
+        products.append([name, price])
+    print(products)
+    return products
 
 # print all information
-for p in products:
-    print(p[0], 'is $', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], 'is $', p[1])
 
 # write file
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-    f.write('商品,價格\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+def main():
+    # file check
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('Yes, you find the file.')
+        products = read_file(filename)
+    else:
+        print('Error, your file is not exist.')
+
+    products = user_input(products)
+    print_products(products)
+    write_file(filename, products)
+
+main()
